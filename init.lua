55f763fcf0d0aacd257e7795b14691d22093216c
Jarl Sondre Sæther <jarl_sondre@hotmail.com>
2024-10-14 11:04:37 +0200

    add autoclosing plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("config")
+require("autoclose").setup()
 
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
@@ -15,7 +16,6 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.expandtab = true
 vim.opt.smarttab = true
-vim.g.pyindent_open_paren = 0 
 
 vim.opt.breakindent = true
 vim.opt.updatetime = 250
